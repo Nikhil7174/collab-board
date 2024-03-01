a38952e1b68d93b9aba8b61305985cedd1f041c2
nikhil7174 <nikhilkumarsingh7174@gmail.com>
2024-03-02 00:42:27 +0530

    whiteboard implemented

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './pages/Home'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Board from "./components/WhiteboardComponent";
 
-function App() {
-  const [count, setCount] = useState(0)
+const CanvasDrawing = () => {
+  const [brushColor, setBrushColor] = useState("black");
+  const [brushSize, setBrushSize] = useState<number>(5);
+
+  useEffect(() => {
+    console.log("CanvasDrawing ", brushSize);
+  }, [brushSize]);
 
   return (
-    <>
-      <h1>Collab-Board</h1>
-      <Home/>
-    </>
-  )
-}
+    <div className="App bg-blue-700 flex flex-col items-center">
+      <div className="font-bold text-3xl">Collaborative Whiteboard</div>
+      <div className="items-center justify-center">
+        <Board brushColor={brushColor} brushSize={brushSize} />
+        <div className="tools flex flex-row p-5 space-x-4 bg-black text-white">
+          <div>
+            <span>Color: </span>
+            <input
+              type="color"
+              value={brushColor}
+              onChange={(e) => setBrushColor(e.target.value)}
+            />
+          </div>
+          <div>
+            <span>Size: </span>
+            <input
+              type="range"
+              color="#fac176"
+              min="1"
+              max="100"
+              value={brushSize}
+              onChange={(e) => setBrushSize(Number(e.target.value))}
+            />
+            <span>{brushSize}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default CanvasDrawing;
--- client/src/components/WhiteboardComponent.tsx
+++ client/src/components/WhiteboardComponent.tsx
@@ -1,162 +1,136 @@
-//@ts-nocheck
-import React, { useEffect, useRef } from 'react';
-import io from 'socket.io-client';
+import React, { useRef, useEffect, useState } from "react";
+import io from "socket.io-client";
 
+interface MyBoard {
+  brushColor: string;
+  brushSize: number;
+}
 
-const WhiteboardComponent = () => {
-  const canvasRef = useRef(null);
-  const testRef = useRef(null);
-  const pc = new RTCPeerConnection();
+const Board: React.FC<MyBoard> = (props) => {
+  const { brushColor, brushSize } = props;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const test = testRef.current;
+    const newSocket = io("http://localhost:5000");
+    console.log(newSocket, "Connected to socket");
+    setSocket(newSocket);
+  }, []);
 
-    canvas.width = 0.98 * window.innerWidth;
-    canvas.height = window.innerHeight;
+  useEffect(() => {
+    if (socket) {
+      // Event listener for receiving canvas data from the socket
+      socket.on("canvasImage", (data) => {
+        // Create an image object from the data URL
+        const image = new Image();
+        image.src = data;
 
-    const ioServer = io.connect("https://dopewhiteboard.herokuapp.com/");
-    const ctx = canvas.getContext("2d");
+        const canvas = canvasRef.current;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        const ctx = canvas.getContext("2d");
+        // Draw the image onto the canvas
+        image.onload = () => {
+          ctx.drawImage(image, 0, 0);
+        };
+      });
+    }
+  }, [socket]);
 
-    let x;
-    let y;
-    let mouseDown = false;
-    let dataChannel;
-    const servers = {
-      iceServers: [
-        {
-          urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
-        },
-      ],
-      iceCandidatePoolSize: 10,
-    };
-    const pc = new RTCPeerConnection(servers);
+  // Function to start drawing
+  useEffect(() => {
+    // Variables to store drawing state
+    let isDrawing = false;
+    let lastX = 0;
+    let lastY = 0;
+    const startDrawing = (e: { offsetX: number; offsetY: number }) => {
+      isDrawing = true;
 
-    console.log("created data channels");
-
-    const applyEvents = () => {
-      dataChannel.onmessage = (e) => {
-        let data = JSON.parse(e.data);
-
-        if (data.draw) {
-          ctx.lineTo(data.draw.x, data.draw.y);
-          ctx.stroke();
-        }
-        if (data.down) {
-          ctx.moveTo(data.down.x, data.down.y);
-        }
-      };
+      console.log(`drawing started`, brushColor, brushSize);
+      [lastX, lastY] = [e.offsetX, e.offsetY];
     };
 
-    const handleMouseDown = (e) => {
-      ctx.moveTo(x, y);
-      if (dataChannel !== undefined) {
-        dataChannel.send(JSON.stringify({ down: { x, y } }));
-      } else {
-        //console.log("not defined");
-      }
-      mouseDown = true;
-    };
+    // Function to draw
+    const draw = (e: { offsetX: number; offsetY: number }) => {
+      if (!isDrawing) return;
 
-    const handleMouseUp = (e) => {
-      mouseDown = false;
-    };
-
-    const handleMouseMove = (e) => {
-      x = e.clientX;
-      y = e.clientY;
-
-      if (mouseDown) {
-        dataChannel.send(JSON.stringify({ draw: { x, y } }));
-        ctx.lineTo(x, y);
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.beginPath();
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
       }
+
+      [lastX, lastY] = [e.offsetX, e.offsetY];
     };
 
-    window.addEventListener('mousedown', handleMouseDown);
-    window.addEventListener('mouseup', handleMouseUp);
-    window.addEventListener('mousemove', handleMouseMove);
+    // Function to end drawing
+    const endDrawing = () => {
+      const canvas = canvasRef.current;
+      const dataURL = canvas.toDataURL(); // Get the data URL of the canvas content
 
-    window.onload = async () => {
-      pc.addEventListener("connectionstatechange", (event) => {
-        if (pc.connectionState === "connected") {
-          //console.log("connected");
-        }
-      });
-
-      pc.ondatachannel = (e) => {
-        console.log("re data channels");
-        dataChannel = e.channel;
-        applyEvents();
-      };
-
-      dataChannel = pc.createDataChannel("test");
-
-      let stream = await navigator.mediaDevices.getUserMedia({ video: true });
-
-      stream.getTracks().forEach((track) => {
-        pc.addTrack(track, stream);
-      });
-
-      const remoteStream = new MediaStream();
-
-      pc.ontrack = (event) => {
-        event.streams[0].getTracks().forEach((track) => {
-          remoteStream.addTrack(track);
-        });
-      };
-
-      test.srcObject = remoteStream;
-
-      //sending the ice candidates
-      pc.onicecandidate = (event) => {
-        if (event.candidate) {
-          //console.log("send ice");
-          ioServer.emit("propogate", { ice: event.candidate });
-        }
-      };
-
-      //sending the offer
-      let offer = await pc.createOffer();
-      await pc.setLocalDescription(offer);
-      //console.log("send offer");
-      ioServer.emit("propogate", {
-        offer: { type: offer.type, sdp: offer.sdp },
-      });
+      // Send the dataURL or image data to the socket
+      // console.log('drawing ended')
+      if (socket) {
+        socket.emit("canvasImage", dataURL);
+        console.log("drawing ended");
+      }
+      isDrawing = false;
     };
 
-    ioServer.on("onpropogate", async (data) => {
-      //console.log("happen");
-      if (data.offer) {
-        //console.log("offer");
-        await pc.setRemoteDescription(new RTCSessionDescription(data.offer));
-        let answer = await pc.createAnswer();
-        await pc.setLocalDescription(answer);
-        ioServer.emit("propogate", { answer });
-      }
-      if (data.answer) {
-        //console.log("answer");
-        await pc.setRemoteDescription(new RTCSessionDescription(data.answer));
-      }
-      if (data.ice) {
-        //console.log("ice");
-        await pc.addIceCandidate(data.ice);
-      }
-    });
+    const canvas: HTMLCanvasElement | null = canvasRef.current;
+    const ctx = canvasRef.current?.getContext("2d");
+
+    // Set initial drawing styles
+    if (ctx) {
+      ctx.strokeStyle = brushColor;
+      ctx.lineWidth = brushSize;
+
+      ctx.lineCap = "round";
+      ctx.lineJoin = "round";
+    }
+    // Event listeners for drawing
+    canvas.addEventListener("mousedown", startDrawing);
+    canvas.addEventListener("mousemove", draw);
+    canvas.addEventListener("mouseup", endDrawing);
+    canvas.addEventListener("mouseout", endDrawing);
 
     return () => {
-      window.removeEventListener('mousedown', handleMouseDown);
-      window.removeEventListener('mouseup', handleMouseUp);
-      window.removeEventListener('mousemove', handleMouseMove);
+      // Clean up event listeners when component unmounts
+      canvas.removeEventListener("mousedown", startDrawing);
+      canvas.removeEventListener("mousemove", draw);
+      canvas.removeEventListener("mouseup", endDrawing);
+      canvas.removeEventListener("mouseout", endDrawing);
     };
-  }, []); // Empty dependency array means this effect will only run once, similar to componentDidMount
+  }, [brushColor, brushSize, socket]);
+
+  const [windowSize, setWindowSize] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
 
   return (
-    <div>
-      <canvas id="canvas" ref={canvasRef}></canvas>
-      <video id="test" ref={testRef}></video>
-    </div>
+    <canvas
+      ref={canvasRef}
+      width={windowSize[0] > 600 ? 600 : 300}
+      height={windowSize[1] > 400 ? 400 : 200}
+      style={{ backgroundColor: "white" }}
+    />
   );
 };
 
-export default WhiteboardComponent;
+export default Board;
