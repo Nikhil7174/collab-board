17a531b190d35bb27c848c07b4da25e30f840df2
sukham23062004 <sukham740@gmail.com>
2024-03-02 18:31:35 +0530

    ImprovedUI

--- client/src/pages/Board.tsx
+++ client/src/pages/Board.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-// import "./App.css";
 import WhiteBoard from "../components/WhiteboardComponent";
 
 const CanvasDrawing = () => {
@@ -17,13 +16,13 @@ const CanvasDrawing = () => {
     setUuid(id);
   };
 
-  const baseUrl = "http://localhost:5175/board";
+  const baseUrl = "http://localhost:5173/board";
 
   const url = baseUrl + "?roomID=" + uuid;
 
   return (
-    <div className="App bg-blue-700 flex flex-col items-center">
-      <div className="font-bold text-3xl m-5 text-white">
+    <div className="App bg-black flex flex-col items-center">
+      <div className="font-semibold text-5xl m-5 text-white font-sans">
         Collaborative Whiteboard
       </div>
       <div className="items-center justify-center mb-9">
@@ -32,26 +31,28 @@ const CanvasDrawing = () => {
           brushSize={brushSize}
           handleUuid={handleUuid}
         />
-        <div className="tools flex flex-row p-5 space-x-4 bg-black text-white">
-          <div>
-            <span>Color: </span>
-            <input
-              type="color"
-              value={brushColor}
-              onChange={(e) => setBrushColor(e.target.value)}
-            />
-          </div>
-          <div>
-            <span>Size: </span>
-            <input
-              type="range"
-              color="#fac176"
-              min="1"
-              max="100"
-              value={brushSize}
-              onChange={(e) => setBrushSize(Number(e.target.value))}
-            />
-            <span>{brushSize}</span>
+        <div className="tools flex flex-row p-5 space-x-4 bg-black text-white justify-between items-center">
+          <div className="flex flex-row justify-center items-center gap-5">
+            <div>
+              <span className="text-center">Color : </span>
+              <input
+                type="color"
+                value={brushColor}
+                onChange={(e) => setBrushColor(e.target.value)}
+              />
+            </div>
+            <div>
+              <span>Size: </span>
+              <input
+                type="range"
+                color="#fac176"
+                min="1"
+                max="100"
+                value={brushSize}
+                onChange={(e) => setBrushSize(Number(e.target.value))}
+              />
+              <span>{brushSize}</span>
+            </div>
           </div>
           <div>
             <button
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="min-h-[100vh]">
+    <div className="DottedBg">
+      <h1 className="Heading text-blue text-center">
+        COLLABORATIVE WHITEBOARD
+      </h1>
       <div className="justify-center items-center flex my-auto min-h-[100vh]">
         <a href="/board" target="_blank" style={{ textDecoration: "none" }}>
-          <button className="px-3 py-2 bg-orange-500 text-white">
+          <button className="px-8 py-4 rounded-[20px] bg-cyan-600 text-white font-semibold text-[30px]">
             Start Board
           </button>
         </a>
