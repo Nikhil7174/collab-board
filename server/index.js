6918b02fea936b0a7a4956d3a282a8dc99676b11
nikhil7174 <nikhilkumarsingh7174@gmail.com>
2024-03-02 16:57:28 +0530

    Implemented roomID

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
+// const { Server } = require("socket.io");
+// const io = new Server({
+//   cors: "http://localhost:5173/",
+// });
+// io.on("connection", function (socket) {
+//   socket.on("canvasImage", (data) => {
+//     socket.broadcast.emit("canvasImage", data);
+//   });
+// });
+
+// io.listen(5000);
+
 const { Server } = require("socket.io");
 const io = new Server({
   cors: "http://localhost:5173/",
 });
+
 io.on("connection", function (socket) {
+  // Get the room ID from the URL
+  const roomID = socket.handshake.query.roomID;
+
+  // Join the room
+  socket.join(roomID);
+
   socket.on("canvasImage", (data) => {
-    socket.broadcast.emit("canvasImage", data);
+    // Broadcast to all clients in the room
+    io.to(roomID).emit("canvasImage", data);
   });
 });
 
