a5a09df443f69d9de7b8fc6f17fd63d4072f6ae6
sukham23062004 <sukham740@gmail.com>
2024-03-03 15:00:14 +0530

    Final2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 //@ts-nocheck
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Board from "./pages/Board";
 import Home from "./pages/Home";
